0e1ab146b6c0fcabccbace0e74c497ebbe637abb
nawan90 <rachmat.d.gunawan@gmail.com>
2022-09-30 16:31:53 +0700

    react color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg';
+import ColorPicker from './components/colorPicker/index.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const title = "Color Picker";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="app-header layout-row align-items-center justify-content-center">
+          <div className="layout-row align-items-center">
+            <img alt="" src={logo} className="logo"/>
+            <h4 id="app-title" data-testid="app-title" className="app-title">{title}</h4>
+          </div>
+        </nav>
+        <ColorPicker colorPickerOptions={this.props.colorPickerOptions} initialSelectedColor={this.props.initialSelectedColor}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/colorPicker/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import "./index.css";
+const classNames = require('classnames');
+
+export default class ColorPicker extends Component {
+
+  render() {
+    let selectedColor;
+    return (
+      <div>
+        <div className="layout-row justify-content-center">
+          <div className="card mt-75">
+            <div className="canvas" data-testid="selectedColor">
+              <p className="text-center mx-auto px-5">{selectedColor}</p>
+            </div>
+            <div className="card-actions">
+              <div className="layout-row justify-content-center align-items-center" data-testid="colorPickerOptions">
+                {this.props.colorPickerOptions.map((color, index) => {
+                  return (
+                    <div
+                      className={
+                        classNames({
+                          'color-box': true,
+                          'mx-8': true,
+                          'my-15': true,
+                          'selected': selectedColor === color
+                        })
+                      }
+                      key={color}
+                    ></div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
