12f549631363b77906dad8d330a6bd6c0d90dc7f
nawan90 <rachmat.d.gunawan@gmail.com>
2022-09-30 17:15:07 +0700

    final react color picker

--- src/components/colorPicker/index.js
+++ src/components/colorPicker/index.js
@@ -1,32 +1,50 @@
 import React, { Component } from "react";
 import "./index.css";
-const classNames = require('classnames');
+const classNames = require("classnames");
 
 export default class ColorPicker extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedColor: "",
+    };
+  }
 
+  handleClick = (index) => {
+    console.log("You Clicked!");
+    this.setState({ selectedColor: this.props.colorPickerOptions[index] });
+  };
   render() {
-    let selectedColor;
     return (
       <div>
         <div className="layout-row justify-content-center">
           <div className="card mt-75">
-            <div className="canvas" data-testid="selectedColor">
-              <p className="text-center mx-auto px-5">{selectedColor}</p>
+            <div
+              className="canvas"
+              data-testid="selectedColor"
+              style={{ backgroundColor: this.state.selectedColor }}
+            >
+              <p className="text-center mx-auto px-5">
+                {this.state.selectedColor}
+              </p>
             </div>
             <div className="card-actions">
-              <div className="layout-row justify-content-center align-items-center" data-testid="colorPickerOptions">
+              <div
+                className="layout-row justify-content-center align-items-center"
+                data-testid="colorPickerOptions"
+              >
                 {this.props.colorPickerOptions.map((color, index) => {
                   return (
                     <div
-                      className={
-                        classNames({
-                          'color-box': true,
-                          'mx-8': true,
-                          'my-15': true,
-                          'selected': selectedColor === color
-                        })
-                      }
+                      onClick={() => this.handleClick(index)}
+                      className={classNames({
+                        "color-box": true,
+                        "mx-8": true,
+                        "my-15": true,
+                        selected: this.state.selectedColor === color,
+                      })}
                       key={color}
+                      style={{ backgroundColor: color }}
                     ></div>
                   );
                 })}
